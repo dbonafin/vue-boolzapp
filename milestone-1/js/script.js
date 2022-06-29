078de23204779866ff7f6cf708e31f8851a4f14d
dbonafin <demianbonafina@gmail.com>
2022-06-29 12:59:26 +0200

    milestone 2 done

--- milestone-1/js/script.js
+++ milestone-1/js/script.js
@@ -1,11 +1,28 @@
-// Milestone 1
+// Milestone 1 - Html Line 55 -
 // Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto.
+
+// Milestone 2 - Html Line 93 -
+// Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// Click sul contatto mostra la conversazione del contatto cliccato.
+
+// Milestone 3
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo..
+
+// Milestone 4
+// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina).
+
+// Milestone 5
+// Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+// Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti.
+
 
 var app = new Vue(
 	{
 		el: "#root",
 		data: {
+			newMessageText: "",
 			activeElement: 0,
 			contacts: [
 				{
@@ -96,7 +113,20 @@ var app = new Vue(
 		methods: {
 			selectChat(index) {
 				this.activeElement = index;
-			}
+			},
+			sendMessage() {
+				if(this.newMessageText.length != 0) {
+				
+					const newMessage =   {
+						date: dayjs().format("DD/MM/YYYY HH:mm:ss"),
+						text: this.newMessageText,
+						status: 'sent'
+					};
+
+					this.contacts[activeElement].messages.push(newMessage);
+					this.newMessageText = ""; 
+				}	
+			}	
 		}
 	}
 );
